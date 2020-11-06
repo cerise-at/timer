be7113fde476a9519331128a21630440bb927424
Cerise Abel Thompson <cerise.athompsone@gmail.com>
2020-11-06 17:24:17 +0000

    updated timer and calculations to be correct, added print function at the start, added print other months functions

--- main.py
+++ main.py
@@ -22,12 +22,8 @@ def select_all_timer(conn):
     cur = conn.cursor()
     cur.execute("SELECT * FROM times")
 
-    #rows = cur.fetchall()
 
-    #for row in rows:
-        #print(row)
-
-def begin(start, end):
+def begin():
     """get user to write start when they start work"""
     ans = input('Do you want to start or end your session? s/e: ')
     if ans == "s":
@@ -38,56 +34,78 @@ def begin(start, end):
             end = datetime.datetime.now()
             print(end)
             return start, end
+        else:
+            return None, None
 
 def month_wage():
     next = input("Would you like to print monthly round up? y/n: ")
     if next == "y":
-        print(pd.read_sql_query("SELECT * FROM times WHERE month = :month ORDER BY day",  conn, params = dict(month=start.strftime("%B"))))
-        print("Total:")
-        print(pd.read_sql_query("SELECT SUM(cost)  FROM times WHERE month = :month ORDER BY day",  conn, params = dict(month=start.strftime("%B"))))
+        which_month = input("Would you like to see this month's wages? y/n: ")
+        if which_month == "y":
+            print(pd.read_sql_query("SELECT * FROM times WHERE month = :month ORDER BY day",  conn, params = dict(month=today.strftime("%B"))))
+            print("Total:")
+            print(pd.read_sql_query("SELECT SUM(cost)  FROM times WHERE month = :month ORDER BY day",  conn, params = dict(month=today.strftime("%B"))))
+        elif which_month == "n":
+            which_month = input("Please the month you wish to see: ")
+            print(pd.read_sql_query("SELECT * FROM times WHERE month = :month ORDER BY day",  conn, params = (which_month,)))
+            print("Total:")
+            print(pd.read_sql_query("SELECT SUM(cost)  FROM times WHERE month = :month ORDER BY day",  conn, params = (which_month,)))
+        else: 
+            print("please try again")
+            month_wage()
     else:
         return
-        
+
+def create_cost():
+    #get cost and hours values
+    hours_total = end - start
+    minutes = hours_total.seconds//60
+    hours = (minutes)//60
+    cost_time = (5 * (round(minutes/5))/60)
+    cost = round((cost_time*15), 2)
+    print("In this session you worked for {} hours and {} minutes, earning £{}".format(hours, (minutes%60), cost))
+   
+    #put into table
+    conn.execute("INSERT INTO times VALUES (:year, :month, :day, :start_t, :end_t, :hours, :minutes, :cost)",\
+    dict(year=start.strftime("%Y"), month = start.strftime("%B"), day = start.strftime("%d"), start_t = start.strftime("%H:%M"),\
+    end_t = end.strftime("%H:%M"), hours = hours, minutes = minutes, cost = cost))
+    conn.commit()
+
+    return (hours, minutes, cost)
+
+    
+def opener():
+    q = input("Would you like to start timer or see monthly invoice? press t / i to continue: ")
+    if q == 't':
+        #start recording time and end recording
+        start, end = begin()
+        hours, minutes, cost = create_cost()
+    elif q == 'i':
+        #ask for month's wages
+        month_wage()
+    else:
+        print("please try again")
+        opener()
+
+    
+
 
 
 if __name__ == '__main__':
 
     conn = create_connection(r"/Users/ceriseabelthompson/documents/code/timer/times.db")
     select_all_timer(conn)
+    today = datetime.datetime.now()
 
-    #initialise start and end
-    start = datetime.datetime.now()
-    end = datetime.datetime.now()
-    print(start)
-
-    #start recording time and end recording
-    begin(start, end)
+    q = input("Would you like to start timer or see monthly invoice? press t / i to continue: ")
+    if q == 't':
+        #start recording time and end recording
+        start, end = begin()
+        hours, minutes, cost = create_cost()
+    elif q == 'i':
+        #ask for month's wages
+        month_wage()
     
-
-    # get values
-    #month = (start.strftime("%B"))
-    #year = (start.strftime("%Y"))
-    #day = (start.strftime("%d"))
-    #start_t = (start.strftime("%H:%M"))
-    #end_t = (end.strftime("%H:%M"))
-
-    #get cost and hours values
-    hours_total = start - end
-    seconds = (24*3600) - hours_total.seconds
-    hours = (seconds/60)/60
-    minutes = seconds%60
-    cost = round((hours*15), 2)
-    print (cost)
-    
-    #put into table
-    #cur = conn.cursor()
-    conn.execute("INSERT INTO times VALUES (:year, :month, :day, :start_t, :end_t, :hours, :minutes, :cost)",\
-    dict(year=start.strftime("%Y"), month = start.strftime("%B"), day = start.strftime("%d"), start_t = start.strftime("%H:%M"),\
-    end_t = end.strftime("%H:%M"), hours = hours, minutes = minutes, cost = cost))
-    conn.commit()
-    #cur.close()
-    #ask for month's wages
-    month_wage()
 
     conn.close()
 
